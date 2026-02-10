86f0a9e8a6aa48fdf70b46387610706b063b6dd5
SaleamlakH <saleamlak.7h@gmail.com>
2026-02-10 17:06:03 +0300

    Feat: Add private method for hashing a key

--- src/hash-map.js
+++ src/hash-map.js
@@ -11,4 +11,19 @@ export class HashMap {
   #array = new Array(16);
   #capacity = 16;
   #loadFactor = 0.75;
+
+  #hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+
+      // prevent from exceeding the maximum integer value allowed in javascript
+      // also keeps it in the array bounds
+      hashCode %= this.#capacity;
+    }
+
+    return hashCode;
+  }
 }
