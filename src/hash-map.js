2f7dd95aa2e0acd417bbd6429d12b7d2c55eab2b
SaleamlakH <saleamlak.7h@gmail.com>
2026-02-10 16:51:45 +0300

    Feat: Add HashMap and Node class with basic properties

--- src/hash-map.js
+++ src/hash-map.js
@@ -1,3 +1,14 @@
-export const testFun = () => {
-  return 'It works';
-};
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+
+export class HashMap {
+  #length = 0;
+  #array = new Array(16);
+  #capacity = 16;
+  #loadFactor = 0.75;
+}
